7279d9d1bb427b25e178c08076e238c1004d830d
yannicklal <yannicklal@free.fr>
2022-12-23 20:57:23 +0100

    Initial commit

---
+++ ContextMenuTest/MainVM.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContextMenuTest
+{
+    public class MainVM : ObservableObject
+    {
+        private string myImagePath;
+
+        public MainVM()
+        {
+            myImagePath = "flower.jpg";
+        }
+
+        public string ImagePath
+        {
+            get { return myImagePath; }
+            set
+            {
+                if (myImagePath == value) return;
+                myImagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+
+        RelayCommand myOpenWindowCommand;
+        public RelayCommand OpenWindowCommand
+        {
+            get
+            {
+                if (myOpenWindowCommand == null)
+                    myOpenWindowCommand = new RelayCommand(OpenWindowCommandAction);
+
+                return myOpenWindowCommand;
+            }
+        }
+
+        private void OpenWindowCommandAction()
+        {
+            NewWindowVM vm = new NewWindowVM();
+            NewWindow view = new NewWindow(vm);
+            vm.OnRequestClose += (s, e) => view.Close();
+
+            view.ShowDialog();
+
+            if(vm.DialogResult)
+            {
+                //Do stuffs
+            }
+        }
+    }
+}
---
+++ ContextMenuTest/NewWindowVM.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContextMenuTest
+{
+    public class NewWindowVM : ObservableObject
+    {
+        public event EventHandler OnRequestClose;
+        private string myText;
+
+        public NewWindowVM()
+        {
+            myText = "The ContextMenu should be closed before I start my instanciation";
+            DialogResult = false;
+        }
+
+        public string Text
+        {
+            get { return myText; }
+            set
+            {
+                if (myText == value) return;
+                myText = value;
+                OnPropertyChanged(nameof(myText));
+            }
+        }
+
+        public bool DialogResult { get; set; }
+
+        RelayCommand myOkCommand;
+        public RelayCommand OkCommand
+        {
+            get
+            {
+                if (myOkCommand == null)
+                    myOkCommand = new RelayCommand(OkCommandAction);
+
+                return myOkCommand;
+            }
+        }
+
+        private void OkCommandAction()
+        {
+            DialogResult = true;
+            OnRequestClose(this, new EventArgs());
+        }
+    }
+}
